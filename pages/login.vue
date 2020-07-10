6ffa46825897dab78871737018f797b690e838f0
thanakornphoto <ita5921@ccollege.ac.th>
2020-07-10 15:41:45 +0700

    login

---
+++ pages/login.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-row
+    align="center"
+    justify="center"
+  >
+    <v-col cols="12">
+      <v-hover
+        v-slot:default="{ hover }"
+        disabled
+      >
+        <v-card
+          :elevation="hover ? 12 : 2"
+          class="mx-auto"
+          height="350"
+          max-width="350"
+          color="#333366"
+        >
+           <v-card-title>
+       <h1>เข้าสู่ระบบ!</h1>
+     </v-card-title>
+     <v-card-text>
+       <v-form>
+         <v-text-field label="Username" prepend-icon="mdi-account-circle" color="red" />
+         <v-text-field 
+            :type="showPassword ? 'text' : 'password'" 
+            label="Password"
+            prepend-icon="mdi-lock"
+            color="red"
+            :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+            @click:append="showPassword = !showPassword"
+          />
+         <v-card-actions>
+         <v-btn @click="doRegister" color="warning" dark large>สมัครสมาชิก</v-btn>
+          <v-spacer></v-spacer>
+         <v-btn @click="doLogin" color="success" dark large>ยืนยัน</v-btn>
+         </v-card-actions>
+       </v-form>
+     </v-card-text>
+        </v-card>
+      </v-hover>
+    </v-col>
+  </v-row>
+</template>
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      showPassword: false
+    }
+  }
+}
+</script>
---
+++ pages/register.vue
@@ -0,0 +1,54 @@
+<template>
+    <v-form
+      ref="form"
+      v-model="valid"
+      :lazy-validation="lazy"
+    >
+      <v-text-field
+        v-model="username"
+        :counter="20"
+        :rules="usernameRules"
+        label="username"
+        required
+      ></v-text-field>
+
+            <v-text-field
+        v-model="password"
+        :counter="20"
+        :rules="nameRules"
+        label="password"
+        required
+      ></v-text-field>
+
+      <v-text-field
+        v-model="email"
+        :rules="emailRules"
+        label="E-mail"
+        required
+      ></v-text-field>
+
+      <v-checkbox
+        v-model="checkbox"
+        :rules="[v => !!v || 'กรุณากดเพื่อบันทึกรหัสผ่าน!']"
+        label="จำรหัสผ่าน?"
+        required
+      ></v-checkbox>
+
+
+      <v-btn
+        color="error"
+        class="mr-4"
+        @click="reset"
+      >
+        ยืนยันการลงทะเบียน
+      </v-btn>
+
+      <v-btn
+        color="warning"
+        @click="resetValidation"
+      >
+       ยกเลิก
+      </v-btn>
+    </v-form>
+  </v-row>
+</template>
