222df2df3ed4d69db8acebb1efc6271a9f7801a7
thanakornphoto <ita5921@ccollege.ac.th>
2020-07-21 17:36:02 +0700

    public

---
+++ layouts/public.vue
@@ -0,0 +1,99 @@
+<template>
+<v-app>
+  <v-card>
+        <v-system-bar dark color="drak">
+      <v-spacer></v-spacer>
+      <v-icon>mdi-wifi-strength-4</v-icon>
+      <v-icon>mdi-signal-cellular-outline</v-icon>
+      <v-icon>mdi-battery</v-icon>
+      <span>12:30</span>
+    </v-system-bar>
+    <v-toolbar
+      color="pink lighten-1"
+      dark
+      flat
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>INFORMATION</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+
+      <template v-slot:extension>
+        <v-tabs
+          v-model="tab"
+          align-with-title
+        >
+          <v-tabs-slider color="yellow"></v-tabs-slider>
+
+          <v-tab v-for="item in items" :key="item">
+            {{ item }}
+          </v-tab>
+        </v-tabs>
+      </template>
+    </v-toolbar>
+
+    <v-tabs-items v-model="tab">
+      <v-tab-item
+        v-for="item in items2"
+        :key="item,i"
+        
+      >
+        <v-card flat>
+          <v-card-text v-text="text"></v-card-text>
+        </v-card>
+      </v-tab-item>
+      <!-- ใส่ข้อความ -->
+  <v-main>
+    <v-container fluid>
+     <v-content>
+       <nuxt/>
+     </v-content>
+    </v-container>
+  </v-main>
+      <v-bottom-navigation
+    :value="activeBtn"
+    color="pink lighten-1"
+    horizontal
+  >
+    <v-btn>
+      <span>Recents</span>
+      <v-icon>mdi-history</v-icon>
+    </v-btn>
+
+    <v-btn>
+      <span>Favorites</span>
+      <v-icon>mdi-heart</v-icon>
+    </v-btn>
+
+    <v-btn>
+      <span>Nearby</span>
+      <v-icon>mdi-map-marker</v-icon>
+    </v-btn>
+  </v-bottom-navigation>
+    </v-tabs-items>
+  </v-card>
+  </v-app>
+</template>
+<script>
+  export default {
+    data () {
+      return {
+        tab: null,
+        items: [
+          'ประวัติส่วนตัว',],
+        items2: [
+          'shopping', 'videos',
+          ],
+      }
+    },
+  }
+</script>
---
+++ pages/me.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-card
+    class="mx-auto"
+    max-width="400"
+  >
+    <v-img
+      class="white--text align-end"
+      height="350px"
+      src="https://scontent.fbkk22-2.fna.fbcdn.net/v/t1.0-9/68631380_2088251108145482_1787979728385212416_o.jpg?_nc_cat=106&_nc_sid=8bfeb9&_nc_eui2=AeEUS7Zg03qksIxOlYz0HUZ_B28faziGQf0Hbx9rOIZB_TNc9LeMW4nagGzHqnh-vHzhwb4YiIBCUivOyN9PeMVn&_nc_ohc=oVWhDBUSxcMAX_VTNy4&_nc_ht=scontent.fbkk22-2.fna&oh=384cfdd8c2b9f68510f45a8392935fd5&oe=5F3B7EC6"
+    >
+      <v-card-title class="red">Thanakorn Suntudna</v-card-title>
+    </v-img>
+
+    <v-card-subtitle class="pb-0">Number 10</v-card-subtitle>
+
+    <v-card-text class="text--primary">
+      <div>Information technology</div>
+
+      <div>25, Moo9 Niwetrat Kangsanamnung 30440</div>
+    </v-card-text>
+
+    <v-card-actions>
+      <v-btn
+        color="blue"
+        text
+      >
+        LIKE
+      </v-btn>
+
+      <v-btn
+        color="red"
+        text
+      >
+        Share
+      </v-btn>
+      <v-chip
+      class="ma-2"
+      color="orange"
+      outlined
+      pill
+    >
+      Follow me
+      <v-icon right>mdi-account-outline</v-icon>
+    </v-chip>
+    </v-card-actions>
+  </v-card>
+</template>
+<script>
+export default {
+   layout:'public',
+  name: 'App',
+  data () {
+    return {
+      showPassword: false
+    }
+  }
+}
+</script>
